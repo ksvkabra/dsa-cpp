9f84002f57766af7a4d1f9e094784cb7b99d9714
Keshav Kabra <ksvkabra@gmail.com>
2020-05-18 07:33:26 +0530

    Implemented kmp pattern matching algorithm

---
+++ string/kmp.cpp
@@ -0,0 +1,89 @@
+// Knuth Morris Pratt pattern searching algorithm
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void kmp(string str, string pattern);
+void lpsArray(vector<int> &lps, string pattern);
+void printArray(vector<int> arr)
+{
+  for (int i : arr)
+  {
+    cout << i << " ";
+  }
+  cout << endl;
+}
+
+int main()
+{
+  kmp("ABABDABACDABABCABAB", "A");
+  return 0;
+}
+
+void kmp(string str, string pattern)
+{
+  int m = pattern.size();
+  int n = str.size();
+
+  vector<int> lps(m);
+  lpsArray(lps, pattern);
+
+  int i = 0;
+  int j = 0;
+
+  while (i < n)
+  {
+    if (pattern[j] == str[i])
+    {
+      i++;
+      j++;
+    }
+
+    if (j == m)
+    {
+      cout << " Found pattern at position " << i - j << endl;
+      j = lps[j - 1];
+    }
+
+    else if (i < n && pattern[j] != str[i])
+    {
+      if (j != 0)
+        j = lps[j - 1];
+      else
+        i = i + 1;
+    }
+  }
+}
+
+void lpsArray(vector<int> &lps, string pattern)
+{
+  int length = 0;
+  lps[0] = 0;
+  int i = 1;
+  int stringLength = pattern.size();
+  while (i < stringLength)
+  {
+    if (pattern[i] == pattern[length])
+    {
+      length++;
+      lps[i] = length;
+      i++;
+    }
+    else
+    {
+      if (length != 0)
+      {
+        length = lps[length - 1];
+      }
+      else
+      {
+        lps[i] = 0;
+        i++;
+      }
+    }
+  }
+  printArray(lps);
+}
