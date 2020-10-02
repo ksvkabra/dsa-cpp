7473c6d37342084bfa12a262252f631fa124ccf8
Keshav Kabra <ksvkabra@gmail.com>
2020-10-02 17:42:01 +0530

    Run prettier and make small changes

--- dynamic-programming/longest-palindromic-subsequence.cpp
+++ dynamic-programming/longest-palindromic-subsequence.cpp
@@ -1,50 +1,71 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-string longestPalindromicSubsequence(string s1, int n) {
+string longestPalindromicSubsequence(string s1, int n)
+{
     string s2 = s1;
     reverse(s1.begin(), s1.end());
 
-    vector<vector<int>> t(n+1, vector<int>(n+1, -1));
+    vector<vector<int>> t(n + 1, vector<int>(n + 1, -1));
 
-
-    for (int i=0; i<n+1; i++) {
-        for (int j=0; j<n+1; j++) {
-            if (i ==0 || j == 0) {
+    for (int i = 0; i < n + 1; i++)
+    {
+        for (int j = 0; j < n + 1; j++)
+        {
+            if (i == 0 || j == 0)
+            {
                 t[i][j] = 0;
             }
         }
     }
 
-    for (int i=1; i<n+1; i++) {
-        for (int j=1; j<n+1; j++) {
-            if (s1[i-1] == s2[j-1]) {
-                t[i][j] = 1 + t[i-1][j-1];
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < n + 1; j++)
+        {
+            if (s1[i - 1] == s2[j - 1])
+            {
+                t[i][j] = 1 + t[i - 1][j - 1];
             }
-            else {
-                t[i][j] = max(t[i-1][j], t[i][j-1]);
+            else
+            {
+                t[i][j] = max(t[i - 1][j], t[i][j - 1]);
             }
         }
     }
 
+    for (vector<int> i : t)
+    {
+        for (int j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
     // backtracking
     string result;
-    int i=n, j=n;
-    while (i >0 && j> 0) {
-        if (s1[i-1] == s2[j-1]) {
-            result.push_back(s1[i-1]);
+    int i = n, j = n;
+    while (i > 0 && j > 0)
+    {
+        if (s1[i - 1] == s2[j - 1])
+        {
+            result.push_back(s1[i - 1]);
             i--;
             j--;
         }
-        else {
-            if (t[i][j-1] > t[i-1][j]) {
+        else
+        {
+            if (t[i][j - 1] > t[i - 1][j])
+            {
                 j--;
             }
-            else {
+            else
+            {
                 i--;
             }
         }
@@ -52,14 +73,16 @@ string longestPalindromicSubsequence(string s1, int n) {
     return result;
 }
 
-int numberOfDeletionsToMakeAStringPalindromic(string s1, int n) {
+int numberOfDeletionsToMakeAStringPalindromic(string s1, int n)
+{
     return n - longestPalindromicSubsequence(s1, n).length();
 }
 
-int main() {
+int main()
+{
     string s = "agbcba";
     int n = s.length();
-    cout<<longestPalindromicSubsequence(s, n)<<endl;
-    cout<<"deletions required to change string to palindrome - "<<numberOfDeletionsToMakeAStringPalindromic(s, n);
+    cout << longestPalindromicSubsequence(s, n) << endl;
+    cout << "deletions required to change string to palindrome - " << numberOfDeletionsToMakeAStringPalindromic(s, n);
     return 0;
 }
--- dynamic-programming/subset-minimum-difference.cpp
+++ dynamic-programming/subset-minimum-difference.cpp
@@ -1,28 +1,35 @@
 // divide the set in two subets, such that the difference between two subsets is minimum
 
-#include<iostream>
-#include<vector>
-#include<climits>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> subSetSum(vector<int> arr, int range, int n) {
-    vector<vector<int>> t(n+1, vector<int>(range+1, INT_MAX));
+vector<int> subSetSum(vector<int> arr, int range, int n)
+{
+    vector<vector<int>> t(n + 1, vector<int>(range + 1, INT_MAX));
     vector<int> possibleSums(0);
-    for (int j=0; j<range+1; j++) {
+    for (int j = 0; j < range + 1; j++)
+    {
         t[0][j] = 0;
     }
-    for (int i=0; i<n+1; i++) {
+    for (int i = 0; i < n + 1; i++)
+    {
         t[i][0] = 1;
     }
-    for (int i=1; i<n+1; i++) {
-        for (int j=1; j<range+1; j++) {
-            if (arr[i-1] <= j) {
-                t[i][j] = t[i-1][j] || t[i-1][j-arr[i-1]];
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j < range + 1; j++)
+        {
+            if (arr[i - 1] <= j)
+            {
+                t[i][j] = t[i - 1][j] || t[i - 1][j - arr[i - 1]];
             }
-            else {
-                t[i][j] = t[i-1][j];
+            else
+            {
+                t[i][j] = t[i - 1][j];
             }
         }
     }
@@ -32,8 +39,10 @@ vector<int> subSetSum(vector<int> arr, int range, int n) {
     //     }
     //     cout<<endl;
     // }
-    for (int i=0; i<(range+1)/2; i++) {
-        if (t[n][i]) {
+    for (int i = 0; i < (range + 1) / 2; i++)
+    {
+        if (t[n][i])
+        {
             possibleSums.push_back(i);
         }
     }
@@ -43,26 +52,32 @@ vector<int> subSetSum(vector<int> arr, int range, int n) {
     return possibleSums;
 }
 
-int minimumSubsetDifference(vector<int> arr, int range, int n) {
+int minimumSubsetDifference(vector<int> arr, int range, int n)
+{
     vector<int> sums = subSetSum(arr, range, n);
     int minimumSum = INT_MAX;
-    for (int i: sums) {
-        minimumSum = min(minimumSum, range-2*i);
+    for (int i : sums)
+    {
+        minimumSum = min(minimumSum, range - 2 * i);
+        cout << i << minimumSum << endl;
     }
     return minimumSum;
 }
 
-int numberSum(vector<int> arr) {
+int numberSum(vector<int> arr)
+{
     int sum = 0;
-    for (int i: arr) {
+    for (int i : arr)
+    {
         sum += i;
     }
     return sum;
 }
 
-int main() {
-    vector<int> arr{ 1, 4, 7 };
+int main()
+{
+    vector<int> arr{1, 4, 7};
     int range = numberSum(arr);
-    cout<<"minimum subset difference is- "<<minimumSubsetDifference(arr, range, 3)<<endl;
+    cout << "minimum subset difference is- " << minimumSubsetDifference(arr, range, 3) << endl;
     return 0;
 }
